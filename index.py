9fdd0d75898b96cd3ac38e82c7cc949235f6d6c6
zhanglingzhi <395393958@qq.com>
2015-02-06 13:56:38 +0800

    index.py commit

---
+++ index.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+print 'please input a number:'
+a = raw_input()
+print a,'+',10,int(a)+10
+
+
+zhanghome = ['zhang','li','wang','wu','yang']
+zhanghome.insert(1,'xiao')
+zhanghome.append('xiaosss')
+''' zhanghome.pop(1) '''
+print zhanghome[-1]
+
+
